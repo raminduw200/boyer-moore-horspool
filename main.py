c4580984c0f2d0e92278788b1095797d31f3855e
Ramindu Walgama <raminduw200@gmail.com>
2022-07-20 22:43:35 +0530

    Functionalise preprocess to improve performance and make incasesensitive

--- main.py
+++ main.py
@@ -8,32 +8,31 @@ import matplotlib.pyplot as plt
 # Reg. No: 2020/CS/195
 # -------------------------------
 
-c = 256
+c = 256  # Number of characters in the alphabet
 
 
-def horspool(text_, pattern_, result_):
-    n = len(text_)
-    m = len(pattern_)
+# Preprocess pattern
+def horspool_preprocess(pattern_, m_):
+    shift_ = [m_] * c
+    for _ in range(m_ - 1):
+        shift_[ord(pattern_[_])] = m_ - _ - 1
+    return shift_
 
-    # Preprocess pattern
-    shift = [m] * c
-    for i in range(m - 1):
-        shift[ord(pattern_[i])] = m - i - 1
 
-    # Search
+# Search
+def horspool_search(text_, n_, pattern_, m_, shift_, result_):
     txt_pos = 0
-    while txt_pos + m <= n:
-        if text_[txt_pos + m - 1] == pattern_[m - 1]:
-            pat_pos = m - 2
+    while txt_pos + m_ <= n_:
+        if text_[txt_pos + m_ - 1] == pattern_[m_ - 1]:
+            pat_pos = m_ - 2
             while pat_pos >= 0 and text_[txt_pos + pat_pos] == pattern_[pat_pos]:
                 pat_pos -= 1
             if pat_pos < 0:
-                result_.append(txt_pos+1)
-        txt_pos += shift[ord(text_[txt_pos + m - 1])]
+                result_.append(txt_pos + 1)
+        txt_pos += shift_[ord(text_[txt_pos + m_ - 1])]
 
 
 if __name__ == '__main__':
-    headers = ['Line', 'Module Code', 'Module Name', 'Matches', 'Indices']
     results = []
     total_matches = 0
 
@@ -41,16 +40,26 @@ if __name__ == '__main__':
     lines = f.readlines()
     f.close()
 
+    pattern = 'Practice'.lower()
+    m = len(pattern)
+
+    shift = horspool_preprocess(pattern, m)
+
     for i in range(len(lines)):
         result = []
         line = lines[i]
-        horspool(line, 'Handling', result)
+        n = len(line)
+        print(pattern.lower() in line.lower())
+        horspool_search(line.lower(), n, pattern, m, shift, result)
         if result:
             line = line.split(' ', 1)
             matches = len(result)
             total_matches += matches
             # Adds search results as rows as in the below format
             # results.append([line_no, code, name, matches, result:(x, y, ...)])
-            results.append([i+1, line[0], line[1], matches, result])
+            results.append([i + 1, line[0], line[1], matches, result])
+
+    headers = ['Line', 'Module Code', 'Module Name', 'Matches', 'Indices']
     print(tabulate(results, headers, tablefmt="fancy_grid"))
-    print(tabulate([['Total Word Matches', total_matches]], ['Total Lines Found   ', len(results)], tablefmt="fancy_grid"))
+    print(tabulate([['Total Word Matches', total_matches]], ['Total Lines Found   ', len(results)],
+                   tablefmt="fancy_grid"))
